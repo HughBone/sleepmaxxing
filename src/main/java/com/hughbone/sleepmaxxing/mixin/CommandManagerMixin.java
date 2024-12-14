4fe8af681314480962b3c12cc73b00043f64db1d
hugh <hugh@netgear.box>
2024-12-14 11:31:02 -0500

    init

---
+++ src/main/java/com/hughbone/sleepmaxxing/mixin/CommandManagerMixin.java
@@ -0,0 +1,63 @@
+package com.hughbone.sleepmaxxing.mixin;
+
+import com.hughbone.sleepmaxxing.Main;
+import net.minecraft.network.packet.c2s.play.CommandExecutionC2SPacket;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ServerPlayNetworkHandler.class)
+public abstract class CommandManagerMixin {
+
+    @Shadow public abstract ServerPlayerEntity getPlayer();
+
+    @Shadow protected abstract void checkForSpam();
+
+    @Inject(method = "onCommandExecution", at = @At(value = "HEAD"), cancellable = true)
+    private void execute(CommandExecutionC2SPacket packet, CallbackInfo ci) {
+        // Return early if not wake cmd
+        String command = packet.command();
+        if (!command.contains(Main.wakeMsg)) return;
+        String[] splitCmd = command.split(" ");
+        if (!splitCmd[0].equals("trigger")) return;
+
+        Text feedbackMsg = Text.literal(splitCmd[1] + " is offline. who you trying to wake up?")
+                .formatted(Formatting.GRAY).formatted(Formatting.ITALIC);
+
+        // Prevent self waking
+        if (this.getPlayer().getNameForScoreboard().equals(splitCmd[1])) {
+            feedbackMsg = Text.literal("why you trying to wake yourself lil bro? are you okay???")
+                    .formatted(Formatting.GRAY).formatted(Formatting.ITALIC);
+        } else {
+            for (ServerPlayerEntity sleeper : this.getPlayer().getServerWorld().getPlayers()) {
+                if (sleeper.getNameForScoreboard().equals(splitCmd[1])) {
+                    if (sleeper.isSleeping()) {
+                        feedbackMsg = Text.literal("you woke up " + sleeper.getNameForScoreboard() + "!")
+                                .formatted(Formatting.GRAY).formatted(Formatting.ITALIC);
+
+                        Text wakeMsg = Text.literal("message from " + this.getPlayer().getNameForScoreboard() + ": " + Main.wakeMsg)
+                                .formatted(Formatting.GRAY).formatted(Formatting.ITALIC);
+
+                        sleeper.sendMessage(wakeMsg, false);
+                        sleeper.wakeUp();
+                    } else {
+                        feedbackMsg = Text.literal(splitCmd[1] + " is already awake. bruh")
+                                .formatted(Formatting.GRAY).formatted(Formatting.ITALIC);
+                    }
+                    break;
+                }
+            }
+        }
+
+        this.getPlayer().sendMessage(feedbackMsg, false);
+        this.checkForSpam();
+        ci.cancel();
+    }
+
+}
---
+++ src/main/java/com/hughbone/sleepmaxxing/mixin/ServerPlayerEntityMixin.java
@@ -0,0 +1,51 @@
+package com.hughbone.sleepmaxxing.mixin;
+
+import com.hughbone.sleepmaxxing.Main;
+import com.mojang.datafixers.util.Either;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.HoverEvent;
+import net.minecraft.text.Text;
+import net.minecraft.util.Unit;
+import net.minecraft.util.math.BlockPos;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(ServerPlayerEntity.class)
+public abstract class ServerPlayerEntityMixin {
+
+    @Shadow public abstract ServerWorld getServerWorld();
+
+    @Inject(method = "trySleep", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;updateSleepingPlayers()V", shift = At.Shift.AFTER))
+    private void sendSleepingStatus(BlockPos pos, CallbackInfoReturnable<Either<PlayerEntity.SleepFailureReason, Unit>> cir) {
+        ServerPlayerEntity player = (ServerPlayerEntity)(Object)this;
+        String randomMsg = Main.sleepMsgs.get(new Random().nextInt(Main.sleepMsgs.size()));
+        int playerTextColor = 0xC1F1FF;
+        int msgTextColor = 0x00FFA6;
+
+        Text playerText = Text.literal(player.getNameForScoreboard())
+            .styled(style -> style.withColor(playerTextColor));
+
+        Text sleepMsgText = Text.literal(" " + randomMsg)
+            .styled(style -> style
+                .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.of("wake")))
+                .withClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND,
+                    "/trigger " + player.getNameForScoreboard() + " " + Main.wakeMsg))
+                .withColor(msgTextColor)
+            );
+
+        sleepMsgText = playerText.copy().append(sleepMsgText);
+
+        for(ServerPlayerEntity serverPlayerEntity : this.getServerWorld().getPlayers()) {
+            serverPlayerEntity.sendMessage(sleepMsgText, false);
+        }
+    }
+
+}
